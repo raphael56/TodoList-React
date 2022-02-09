c492b55761aabac7cc7b7b177082400b74bd4974
raphael56 <raphael.hervieux@gmail.com>
2022-02-09 17:25:12 +0100

    Create first-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import './App.css';
+import '../node_modules/bulma/css/bulma.min.css'
+import './components/header/header'
+import Header from './components/header/header';
+
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+ import React from 'react';
+ 
+ function Header(){
+     return (
+         <nav className="has-text-centered navBar has-background-danger " role="navigation" aria-label="main navigation">
+             <h2 className="is-size-4 has-text-white " style={{lineHeight:'52px'}}>
+                 App To Do List !
+             </h2>
+      </nav>
+     );
+ }
+ 
+ export default Header;
+ 
